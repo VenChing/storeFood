6f32edc4bfbed4ebbfe28cf5a04f203b82d8706a
Hdiebk <34081472+hdiebk@users.noreply.github.com>
2020-08-08 13:33:16 +0800

    安装vant并引入/全局/按需

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
+/*
+ * @Description: Web project
+ * @Version: 2.0
+ * @Autor: Xu.wenqing
+ * @Email: 2510129345@qq.com
+ * @Date: 2020-08-08 13:23:30
+ * @LastEditors: Xu.wenqing
+ * @LastEditTime: 2020-08-08 13:32:29
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// 引入vant
+import vant from 'vant';
+import 'vant/lib/index';
+//全局使用
+Vue.use(vant);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+	el: '#app',
+	router,
+	components: { App },
+	template: '<App/>'
+});
